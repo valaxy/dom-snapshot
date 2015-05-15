f310be852a168cf7335fb66a266fb62683fa7171
valaxy <valaxy@qq.com>
2015-05-15 10:45:11 +0800

    add unit test

---
+++ test/unit/test.js
@@ -0,0 +1,6 @@
+define(function (require) {
+	//require('../model/element-node-data')
+	//require('../model/text-node-data')
+	require('../document-capture/id-generator')
+	require('../document-capture/capture')
+})
